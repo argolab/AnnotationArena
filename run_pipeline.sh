3a727e4e6954107ac7501235bd7dd3a6f3706bb9
Prabhav55221 <prabhavsingh55221@gmail.com>
2025-06-20 21:19:40 -0400

    Minor cleanups. Forgot to add total loss.

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -8,8 +8,7 @@
 #SBATCH --nodes=1
 #SBATCH --mem-per-cpu=12GB
 #SBATCH --gpus=1
-#SBATCH --partition=gpu-a100
-#SBATCH --account=a100acct
+#SBATCH --partition=gpu
 #SBATCH --mail-user="psingh54@jhu.edu"
 
 source /home/psingh54/.bashrc
